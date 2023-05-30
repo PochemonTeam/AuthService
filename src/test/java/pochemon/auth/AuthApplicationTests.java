0eb5fd1a7c7df1a09fa45fe05ca76e945d2f3f1d
GAUCHOUX Xavier <xavier.gauchoux@soprasteria.com>
2023-05-30 14:05:02 +0200

    First

---
+++ src/test/java/pochemon/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package pochemon.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

95ce33bc20d993cde4e458f59b47d000957e3b87
Martin <martin.jourjon42@gmail.com>
2023-06-07 15:12:23 +0200

    maj auth

--- src/main/java/pochemon/auth/controller/AuthController.java
+++ src/main/java/pochemon/auth/controller/AuthController.java
@@ -1,16 +1,16 @@
 package pochemon.auth.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import pochemon.auth.service.AuthService;
 import pochemon.dto.AuthDTO;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -23,11 +23,13 @@ public class AuthController {
 
     @PostMapping("/register")
     public boolean addLogin(@RequestBody AuthDTO loginRequest) {
+        log.info("AuthController : New register request for {}", loginRequest.getUsername());
         return authService.addLogin(loginRequest.getUsername(), loginRequest.getPassword());
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody AuthDTO loginRequest) {
+        log.info("AuthController : New login request for {}", loginRequest.getUsername());
         // Vérifier les informations d'identification de l'utilisateur
         if (authService.authenticate(loginRequest.getUsername(), loginRequest.getPassword())) {
             // Générer un token JWT
@@ -36,13 +38,17 @@ public class AuthController {
             return ResponseEntity.ok(token);
         } else {
             // Les informations d'identification sont incorrectes
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Identifiants invalides");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Identifiants invalides");
         }
     }
 
-    @PostMapping("/verify")
-    public boolean validateToken(String token) {
-        return authService.validateToken(token);
+    @PostMapping
+    @GetMapping
+    public ResponseEntity<Boolean> authenticate(@RequestHeader("Authorization") String token) {
+        if(authService.validateToken(token)) {
+            return ResponseEntity.ok(true);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
 }
